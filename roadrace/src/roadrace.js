7f4dd70ce797f507e7e734fd7d30e32ed25e67be
Catlyn Bowles <bowlescatlyn@gmail.com>
2023-07-17 15:35:04 -0600

    roadrace

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,19 @@
 class Roadrace {
+  constructor({ title, city }) {
+    this.name = title;
+    this.location = city;
+    this.distance = null;
+    this.participants = []
+  }
 
+  setDistance(distance) {
+    this.distance = distance;
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`;
+  }
+
+  registerParticipants() {
+    
+  }
 }
 
 module.exports = Roadrace;
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,25 @@
 class Runner {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
 
+  runSomeMiles(miles) {
+    this.fitness += miles;
+    this.milesRun += miles;
+  }
+
+  stretch() {
+    this.fitness += 0.5;
+  }
+
+  runRace(name, length) {
+    this.completedRaces.push(name);
+    this.runSomeMiles(length);
+  }
 }
 
 module.exports = Runner;
