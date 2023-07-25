1d8f225a64a8ee4334c04d18ae81405d75a7b0d4
Catlyn Bowles <bowlescatlyn@gmail.com>
2023-07-24 19:48:23 -0600

    vending mach

--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -7,15 +7,24 @@ class VendingMachine {
 
   addSnacks(snack) {
     if (!this.snacks.find((snak) => snak.name === snack.name)) {
-      this.snacks.push(snack)
+      this.snacks.push(snack);
     } else {
       return "Sorry, that snack is already stocked! Try adding a different snack.";
     }
   }
 
   purchaseSnack(name, price) {
-    const selectedSnack = this.snacks.find(snack => snack.name === name)
-    selectedSnack.removeItem()
+    const selectedSnack = this.snacks.find((snack) => snack.name === name);
+    const snackPrice = selectedSnack.price;
+    if (price < snackPrice) {
+      return "Sorry, not enough payment. Please add more money.";
+    }
+    if (selectedSnack.itemsInStock > 0) {
+      selectedSnack.removeItem();
+      return `Success! Here is $${price - snackPrice} back!`;
+    } else {
+      return "Sorry, no items in stock. Try another item.";
+    }
   }
 }
 
